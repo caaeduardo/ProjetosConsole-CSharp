cd7154a3cb435a786c1829f5bf99440cccd6fee3
caaeduardo <carloseduardosilva419@gmail.com>
2024-10-09 21:29:45 -0300

    Primeira versão do projeto console (lista 1)

---
+++ ProjetosConsole/Lista3.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetosConsole
+{
+    internal class Lista3
+    {
+        internal void Exe1()
+        {
+            Console.WriteLine("Exercício 1");
+
+            Console.WriteLine("Fim do exercício 1");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+
+
+        internal void Exe2()
+        {
+            Console.WriteLine("Exercício 2");
+
+            Console.WriteLine("Fim do exercício 2");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+
+        internal void Exe3()
+        {
+            Console.WriteLine("Exercício 3");
+
+            Console.WriteLine("Fim do exercício 3");
+            Console.WriteLine("-------------------------------------------------------------");
+        }
+    }
+}
